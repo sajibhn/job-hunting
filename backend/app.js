1828c951584e1b14126d320a7fd09b5b1b01c100
Sajib Hossain <sajibhossain1185@gmail.com>
2022-07-20 19:17:09 +0600

    added signup functionality

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,21 @@
 import express from 'express'
-import cors from 'cors'
 import mongoose from 'mongoose'
+import router from './routes/user-routes.js';
+import cors from 'cors'
+import dotenv from 'dotenv'
+const app = express();
 
-const app = express()
+dotenv.config()
+app.use(cors())
+app.use(express.json())
+app.use('/api/user', router)
 
-app.get('/', (req, res) => {
-    res.send("hello world")
+const MONGO_URL = process.env.MONGO_URL
+const PORT = process.env.PORT || 5000
+mongoose.connect(`${MONGO_URL}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-
-app.listen(5000)
+    .then(() => app.listen(PORT))
+    .then(() => console.log('connected db and port'))
+    .catch((err) => console.log(err))
