bfa12ad5699b65113349ce4fcfcf4300d7fd045b
Sajib Hossain <sajibhossain1185@gmail.com>
2022-07-21 22:50:00 +0600

    added delete functionality

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -19,14 +19,25 @@ const Jobs = () => {
 
     useEffect(() => {
         sendRequest().then((data) => setJobs(data.user.jobs))
-    }, [])
+    }, [sendRequest])
 
+    const deleteRequest = async (del_id) => {
+        const res = await axios
+            .delete(`http://localhost:5000/api/jobs/delete/${del_id}`)
+            .catch((err) => console.log(err));
+        const data = await res.data;
+        return data;
+    };
+    const handleDelete = (del_id) => {
+        deleteRequest(del_id)
+            .then(() => navigate("/"))
+    };
 
     return (
         <div className="container" style={{ marginTop: "100px" }}>
             <div className='d-flex gap-5'>
                 <h3>All jobs</h3>
-                <button class="btn btn-primary" onClick={() => navigate("/addjob")}><i class="fa fa-plus"></i> Add New</button>
+                <button className="btn btn-primary" onClick={() => navigate("/addjob")}><i className="fa fa-plus"></i> Add New</button>
             </div>
 
             {jobs && jobs.map((job, index) => {
@@ -37,6 +48,7 @@ const Jobs = () => {
                             <h5 className='lead'>{job?.position}</h5>
                             <a href={job?.link} target="_blank" rel='noreferrer' className='d-block text-dark text-decoration-none'>Link</a>
                             <button onClick={() => { navigate(`/updatejob/${job?._id}`) }} className='btn btn-primary mt-3'>Edit</button>
+                            <button onClick={() => handleDelete(job._id)} className='btn btn-warning mt-3 ms-2'>delete</button>
                         </div>
                     </div>
                 )
